634fd021185936e729c68097b45131089d66cce7
Stefan Tomm <stomm@meshcloud.io>
2017-11-06 14:04:59 +0100

    initial project creation

---
+++ src/main/java/de/meshcloud/example/springkotlin/SpringKotlinApplication.java
@@ -0,0 +1,12 @@
+package de.meshcloud.example.springkotlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKotlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKotlinApplication.class, args);
+	}
+}
