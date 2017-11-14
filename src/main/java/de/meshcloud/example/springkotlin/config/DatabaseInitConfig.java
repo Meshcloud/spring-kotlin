4293bbfbb4f6dfa5eb24fa91134eb02874550b64
Stefan Tomm <stefantomm@gmail.com>
2017-11-14 08:23:21 +0100

    use a configuration for the project name

--- src/main/java/de/meshcloud/example/springkotlin/config/DatabaseInitConfig.java
+++ src/main/java/de/meshcloud/example/springkotlin/config/DatabaseInitConfig.java
@@ -5,6 +5,7 @@ import de.meshcloud.example.springkotlin.model.Resource;
 import de.meshcloud.example.springkotlin.model.ResourceType;
 import de.meshcloud.example.springkotlin.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,9 @@ import java.util.Arrays;
 
 @Component
 public class DatabaseInitConfig implements ApplicationRunner {
+
+    @Value("${app.projectName}")
+    private String projectName;
 
     @Autowired
     private ProjectRepository projectRepository;
@@ -25,8 +29,8 @@ public class DatabaseInitConfig implements ApplicationRunner {
 
     private void initDB() {
         Project project = new Project();
-        project.setName("P1");
-        project.setDescription("My first project");
+        project.setName(projectName);
+        project.setDescription("Just a test project");
 
         Resource resourceCF = new Resource(
                 "Cloud Foundry",
