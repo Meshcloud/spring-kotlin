601c221ab90a304b0f0b245fd573565b4b8bd5d7
Stefan Tomm <stefantomm@gmail.com>
2017-11-14 08:15:40 +0100

    split ProjectController and ProjectListController

--- src/main/java/de/meshcloud/example/springkotlin/web/ProjectListController.java
+++ src/main/java/de/meshcloud/example/springkotlin/web/ProjectListController.java
@@ -1,5 +1,6 @@
 package de.meshcloud.example.springkotlin.web;
 
+import de.meshcloud.example.springkotlin.dto.ProjectListDTO;
 import de.meshcloud.example.springkotlin.repositories.ProjectRepository;
 import de.meshcloud.example.springkotlin.services.CostCalculationService;
 import de.meshcloud.example.springkotlin.util.PerformanceLogger;
@@ -8,28 +9,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("projects")
-public class ProjectController {
-
-    private CostCalculationService costCalculationService;
+public class ProjectListController {
 
     private ProjectRepository projectRepository;
 
-    public ProjectController(
-            CostCalculationService costCalculationService,
+    public ProjectListController(
             ProjectRepository projectRepository
     ) {
-        this.costCalculationService = costCalculationService;
         this.projectRepository = projectRepository;
     }
 
-    @GetMapping("{projectId}/costs")
-    public Double calculateCosts(@PathVariable("projectId") Long projectId) {
-        return PerformanceLogger.logPerformance(
-                () -> costCalculationService.calculateCosts(projectId),
-                "Cost Calculation for " + projectId
-        );
+    @GetMapping("overview")
+    public List<ProjectListDTO> overview() {
+        return projectRepository
+                .findAll()
+                .stream()
+                .map(p -> new ProjectListDTO(p))
+                .collect(Collectors.toList());
     }
-
 }
