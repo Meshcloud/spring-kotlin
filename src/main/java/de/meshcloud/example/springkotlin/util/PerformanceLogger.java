c04c4569248d6ef81b5612ffdeab967457b254c2
Stefan Tomm <stefantomm@gmail.com>
2017-11-14 20:55:11 +0100

    add logFormat method to show extension function in kotlin

--- src/main/java/de/meshcloud/example/springkotlin/util/PerformanceLogger.java
+++ src/main/java/de/meshcloud/example/springkotlin/util/PerformanceLogger.java
@@ -3,6 +3,7 @@ package de.meshcloud.example.springkotlin.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.function.Supplier;
 
@@ -10,12 +11,18 @@ public class PerformanceLogger {
 
     private static Logger log = LoggerFactory.getLogger(PerformanceLogger.class);
 
+    private static SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
     // In Java there are Suppliers, Consumers, Predicates, BiConsumers, etc
     // see https://docs.oracle.com/javase/8/docs/api/java/util/function/package-summary.html
     public static <T> T logPerformance(Supplier<T> fn, String name) {
-        log.info(String.format("starting '%s' at %tc", name, new Date()));
+        log.info(String.format("starting '%s' at %s", name, logFormat(new Date())));
         T result = fn.get();
-        log.info(String.format("finished '%s' at %tc", name, new Date()));
+        log.info(String.format("finished '%s' at %s", name, logFormat(new Date())));
         return result;
+    }
+
+    private static String logFormat(Date date) {
+        return df.format(date);
     }
 }
